99e05be4dcfcde8e81934fd35acd78b20717fdac
powersparks <powersparks@gmail.com>
2016-05-22 21:55:39 -1000

    continue setup of solution.... container and application setup...  builds.. but doesn't do anything yet

--- te.extension.kharta/Plugins/Applications.cs
+++ te.extension.kharta/Plugins/Applications.cs
@@ -3,10 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using Telligent.Evolution.Components;
+using Telligent.DynamicConfiguration.Components;
+using Telligent.Evolution.Extensibility.Api.Entities.Version1;
+using Telligent.Evolution.Extensibility.Api.Version1;
+using Telligent.Evolution.Extensibility.Security.Version1;
+using Telligent.Evolution.Extensibility.Urls.Version1;
+using Telligent.Evolution.Extensibility.Version1;
+using Telligent.Evolution.Urls.Routing;
+using TEApi = Telligent.Evolution.Extensibility.Api.Version1.PublicApi;
+using UIApi = Telligent.Evolution.Extensibility.UI.Version1;
+using Permission = Telligent.Evolution.Extensibility.Security.Version1.Permission;
+ 
+ 
+using Telligent.Evolution.Extensibility;
 
+using Telligent.Evolution.Extensibility.Content.Version1;
 namespace te.extension.kharta
 {
-    public class Applications
+    public class Applications : IPlugin, IPluginGroup, IApplications, IConfigurablePlugin
     {
+        public readonly Guid Applications_id = new Guid("5127c319-fda6-40e4-bfd7-37bbfef3ba39");
+        public readonly Guid ApplicationsType_id = new Guid("e504f58d-c1d8-40a8-bf55-bc38c65625e9");
+
+        #region IPlugin
+
+        public string Description
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #region IPluginGroup
+        public IEnumerable<Type> Plugins
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+ 
+
+        #region IConfigurablePlugin
+        public PropertyGroup[] ConfigurationOptions
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Guid DataTypeId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        
+
+        public void Update(IPluginConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+        #region IApplications
+
+        public IApplicationEvents Events
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public Application Get([Documentation("Application Id")] Guid applicationId, [Documentation("Application type Id")] Guid applicationTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApiList<Application> List(Guid containerTypeId, Guid containerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApiList<Application> Search(Guid containerTypeId, Guid containerId, string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        #endregion
+
+
+
+
+        #endregion
+
     }
 }
--- te.extension.kharta/Plugins/Containers.cs
+++ te.extension.kharta/Plugins/Containers.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Telligent.DynamicConfiguration.Components;
+using Telligent.Evolution.Extensibility.Api.Entities.Version1;
+using Telligent.Evolution.Extensibility.Api.Version1;
+using Telligent.Evolution.Extensibility.Version1;
+ 
 
 namespace te.extension.kharta.Plugins
 {
-    class Containers
+    public class Containers : IPlugin, IPluginGroup, IContainers, IConfigurablePlugin
     {
+        public readonly Guid ContainerType_id = new Guid("4ebcc1b3-9daa-4550-aca5-77bb13d026d0");
+        public readonly Guid Containers_id = new Guid("5127c319-fda6-40e4-bfd7-37bbfef3ba39");
+        public readonly Guid ContainersType_id = new Guid("e504f58d-c1d8-40a8-bf55-bc38c65625e9");
+
+        #region IPlugin
+      
+        public string Description
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #region IPluginGroup
+        public IEnumerable<Type> Plugins
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+        #region IContainers
+        public IContainerEvents Events
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public Guid DataTypeId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public Container Get([Documentation("Container Id")] Guid containerId, [Documentation("Container type Id")] Guid containerTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #endregion
+
+        #region IConfigurablePlugin
+        public PropertyGroup[] ConfigurationOptions
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public void Update(IPluginConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        
+
+
+        #endregion
     }
 }
